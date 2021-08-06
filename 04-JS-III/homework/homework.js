6c8f32caa25a20fe2ccf39b3a7136136f083d6f0
fernando ruiz parietti <ferdyjrp@yahoo.com.ar>
2021-08-06 19:45:39 -0300

    HW 04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -148,9 +148,8 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  if(numeroDeDia===1 || numeroDeDia===7){
-    return "Es fin de semana";
-  }
+  if(numeroDeDia===1 || numeroDeDia===7) return "Es fin de semana"
+  
   return "Es dia Laboral";
 } 
 
@@ -159,8 +158,8 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  let num = n.toString()
-  if (num.charAt(0)==="9") {
+  let num = n.toString() // transformamos el numero en un string 
+  if (num.charAt(0)==="9") { // me fijo si el primer caracter del string es ===9
     return true;
   }
   return false;
@@ -222,9 +221,9 @@ function breakStatement(numero) {
   // Tu código:
   var array = [];
   var suma = numero;
-  for(var i =0 ; i<10 ; i++){
-      suma= suma+2;
-      if(suma==i) break
+  for(var i =0 ; i<10 ; i++){ 
+      suma= suma+2;  // suma=suma+=2   <=== Es lo mismo.
+      if(suma===i) break // el break interrumpe el ciclo for.
       else{
         array.push(suma);
       }
@@ -246,8 +245,8 @@ function continueStatement(numero) {
   // Tu código:
   var array = [];
   var suma = numero;
-  for(var i= 0; i<10; i++) {
-    if(i === 5) continue;
+  for(var i= 0; i<10; i++) { 
+    if(i === 5) continue; // el continue, pasa al paso siguiente del for (saltea un paso)
     else {
       suma = suma + 2;
       array.push(suma);
